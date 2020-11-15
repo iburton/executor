a01055b7fb3918bf78fbfcdd4045d43631a106c0
Ira Burton <ira.burton@gmail.com>
2020-11-15 09:04:54 -0700

    :wrench: Added various linting and setup files

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  printWidth: 120,
+
+  svelteSortOrder : "styles-scripts-markup",
+  svelteStrictMode: false,
+  svelteBracketNewLine: false,
+  svelteAllowShorthand: true,
+  svelteIndentScriptAndStyle: true
+}
