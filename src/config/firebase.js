fc9a297ded0e267f56fd27873f5a665d390d8b38
Ira Burton <ira.burton@gmail.com>
2020-11-17 09:56:45 -0700

    :wrench: Added configuration files

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+/**
+ * @overview Initialize firebase
+ * @author Ira Burton <ira.burton+gh@gmail.com>*
+ */
+
+import firebase from 'firebase/app'
+import 'firebase/firebase-firestore'
+import 'firebase/firebase-auth'
+import 'firebase/firebase-functions'
+import 'firebase/firebase-storage'
+import firebaseConfig from './firebase_settings'
+
+firebase.initializeApp(firebaseConfig)
+
+const Firestore = firebase.firestore()
+const Auth = firebase.auth()
+const Functions = firebase.app().functions('europe-west1')
+const Storage = firebase.storage()
+
+export { Firestore, Auth, Functions, Storage }
